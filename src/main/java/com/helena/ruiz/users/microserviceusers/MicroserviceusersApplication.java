f0c3636a1029ce208ffdc20930b79bef9908138e
pinkiwinkie <elrumor99@gmail.com>
2024-04-23 13:08:54 +0200

    connection bbdd ok

---
+++ src/main/java/com/helena/ruiz/users/microserviceusers/MicroserviceusersApplication.java
@@ -0,0 +1,13 @@
+package com.helena.ruiz.users.microserviceusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceusersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helena/ruiz/users/microserviceusers/MicroserviceusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helena.ruiz.users.microserviceusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
